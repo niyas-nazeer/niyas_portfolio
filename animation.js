ddf407b586eaf259b5f85852d0dec39f05a208b4
niyas-nazeer <niyasnazeer719@gmail.com>
2024-02-07 12:55:15 +0530

    face change 7

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 new TypeIt("#element", {
-    strings: ["NIYAS NAZEER","I'M A FULL STACK DEVELOPER"],
+    strings: ["NIYAS NAZEER"," ","I'M A FULL STACK DEVELOPER"],
     speed: 100,
     loop: false,
     }).go();
