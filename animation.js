66e781536585f11f6c3dfd38b7763076045d1d30
Niyas Nazeer <157343533+niyas-nazeer@users.noreply.github.com>
2024-05-24 11:04:17 +0530

    animation speed changed 75%

--- animation.js
+++ animation.js
@@ -1,11 +1,11 @@
 new TypeIt("#element", {
     strings: ["NIYAS NAZEER"," ","I'M A FULL STACK DEVELOPER"],
-    speed: 100,
+    speed: 75,
     loop: false,
     }).go();
 
     new TypeIt("#project", {
     strings: ["Coming Soon .....!"],
-    speed: 100,
+    speed: 75,
     loop: true,
     }).go();
